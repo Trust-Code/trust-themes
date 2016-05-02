d816263e5571d1981c7a5816811788e3ac64fdb1
Danimar Ribeiro <danimaribeiro@gmail.com>
2016-05-02 11:05:47 -0300

    Novo site

---
+++ theme_alb/controllers/main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#                                                                             #
+# Copyright (C) 2016 Trustcode - www.trustcode.com.br                         #
+#              Danimar Ribeiro <danimaribeiro@gmail.com>                      #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU Affero General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+###############################################################################
+
+
+import re
+from datetime import datetime
+from openerp.addons.web import http
+from openerp.addons.web.http import request
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
+from openerp.addons.website.models.website import slug
