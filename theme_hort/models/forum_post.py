286b24f6e047643b004cdeb37984da67a78c9fe5
Danimar Ribeiro <danimaribeiro@gmail.com>
2016-05-30 13:21:51 -0300

    Novo metodo para montar a url do post

--- theme_hort/models/forum_post.py
+++ theme_hort/models/forum_post.py
@@ -1,6 +1,7 @@
 
 
 from openerp import api, models
+from openerp.addons.website.models.website import slug
 
 
 class ForumPost(models.Model):
@@ -10,3 +11,9 @@ class ForumPost(models.Model):
     def last_comment(self):
         last_message = self.message_ids.sorted(key=lambda r: r.id)[0]
         return last_message.body
+
+    @api.one
+    def url_post(self):
+        base_url = self.env['ir.config_parameter'].get_param('web.base.url')
+        return '%s/forum/%s/question/%s' % (base_url, slug(self.forum_id),
+                                            slug(self))
