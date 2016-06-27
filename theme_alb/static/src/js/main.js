c2ef236237ded7d89e8db86a01af01c61d63032a
Danimar Ribeiro <danimaribeiro@gmail.com>
2016-06-27 16:00:27 -0300

    Diminuição do tamanho do slider

--- theme_alb/static/src/js/main.js
+++ theme_alb/static/src/js/main.js
@@ -10,5 +10,22 @@
                 window.location = suggestion.url;
             }
         });
+
+        var slider = new MasterSlider();
+        // adds Arrows navigation control to the slider.
+        slider.control('arrows');
+        //slider.control('timebar' , {insertTo:'#masterslider'});
+        slider.control('bullets');
+
+         slider.setup('masterslider' , {
+             width:1400,    // slider standard width
+             height:500,   // slider standard height
+             space:1,
+             layout:'fullwidth',
+             loop:true,
+             preload:0,
+             instantStartLayers:true,
+             autoplay:true
+        });
     });
 })(jQuery);
